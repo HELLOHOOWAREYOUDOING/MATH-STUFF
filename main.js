8869859942351cbf2c52e9018adfe2d72f9d5f0d
HELLOHOOWAREYOUDOING <119167987+HELLOHOOWAREYOUDOING@users.noreply.github.com>
2022-11-27 12:50:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+function add()  {
+   var result =
+   Number(document.getElementById("firstnumber").value) +
+   Number(document.getElementById("secondnumber").value) ;
+   console.log(result);alert(result);
+}
+
+function mul()  {
+   var result =
+   Number(document.getElementById("firstnumber").value) *
+   Number(document.getElementById("secondnumber").value) ;
+   console.log(result);alert(result);
+}
+function sub()  {
+   var result =
+   Number(document.getElementById("firstnumber").value) -
+   Number(document.getElementById("secondnumber").value) ;
+   console.log(result);alert(result);
+}
+
+function div()  {
+   var result =
+   Number(document.getElementById("firstnumber").value) /
+   Number(document.getElementById("secondnumber").value) ;
+   console.log(result);alert(result);
+}
